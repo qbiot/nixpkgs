0711d259f44c92d4883d1221e5aa8ab1ed91171f
Matthew Bauer <mjbauer95@gmail.com>
2019-02-24 13:16:46 -0500

    nix: disable aws in static
    aws libs fail to link statically

--- pkgs/top-level/static.nix
+++ pkgs/top-level/static.nix
@@ -163,7 +163,7 @@ in {
     gssSupport = false;
   };
 
-  nix = super.nix.overrideAttrs (_: {
+  nix = (super.nix.override { withAWS = false; }).overrideAttrs (o: {
     NIX_LDFLAGS = "-lssl -lbrotlicommon -lssh2 -lz -lnghttp2 -lcrypto";
   });
 }
